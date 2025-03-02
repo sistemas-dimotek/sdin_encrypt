0c8fae30a8d9d7456c2172813fad8eb81b9c0801
rel-dimotek <ricardo.estrada@dimotek.biz>
2025-03-01 21:54:14 -0700

    im out of ideas 5

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from ast import literal_eval
-import schedule
+import schedule as sch
 import time
 import xmlrpc.client
 import json
@@ -170,7 +170,7 @@ def programar_tareas_diarias():
     logger.info("Programando tareas diarias")
 
     # Limpiar todas las tareas programadas anteriormente
-    # schedule.clear()
+    sch.clear()
 
     # Programar cada categoría con un intervalo de 20 minutos
     for i, categoria_id in enumerate(CATEGORIAS_IDS):
@@ -183,10 +183,10 @@ def programar_tareas_diarias():
         logger.info(f"Programando categoría {categoria_id} para ejecutarse a las {tiempo_ejecucion}")
 
         # Programar la tarea a una hora específica
-        schedule.every().day.at(tiempo_ejecucion).do(consultar_y_enviar, categoria_id=categoria_id)
+        sch.every().day.at(tiempo_ejecucion).do(consultar_y_enviar, categoria_id=categoria_id)
 
     # Programar la función para reprogramar las tareas al día siguiente
-    schedule.every().day.at("00:00").do(programar_tareas_diarias).tag('daily')
+    sch.every().day.at("00:00").do(programar_tareas_diarias).tag('daily')
 
 
 # Función principal
@@ -200,7 +200,7 @@ def main():
 
         # Bucle principal para ejecutar las tareas programadas
         while True:
-            schedule.run_pending()
+            sch.run_pending()
             time.sleep(60)  # Verificar cada minuto en lugar de cada segundo para reducir carga
 
     except KeyboardInterrupt:
